ec99659bd92ab2dbafc67d6cca2c58e8cea8d31a
Niranjan753 <niranjanr753@gmail.com>
2025-12-23 18:56:27 +0530

    add vercel origin

--- lib/auth.ts
+++ lib/auth.ts
@@ -12,6 +12,7 @@ export const auth = betterAuth({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         },
+        // add vercel origin
         github: {
             clientId: process.env.GITHUB_CLIENT_ID!,
             clientSecret: process.env.GITHUB_CLIENT_SECRET!
